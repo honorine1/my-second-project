8f2cbfddae294050df5d169969d7f60f01dd7aa2
honorine1 <uwurukundoh@gmail.com>
2019-07-18 13:05:38 +0200

    html form is added

---
+++ custom.js
@@ -0,0 +1,8 @@
+
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+console.log(maleNames,  femaleNames,  daysOfWeek);
+
+// var d =  parseInt(( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
+// console.log(result);
